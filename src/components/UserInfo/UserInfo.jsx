b10deccc263cac4de4509910d2e1d6e38d8f8aec
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-08-04 10:21:19 -0300

    1 Commit

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,12 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+// export const UserInfo = () => <>UserInfo markup</>;
+
+export const UserInfo = ({ todo }) => (
+  <div className="UserInfo">
+    {todo.user && (
+      <div className="UserInfo__user" data-cy="user">
+        <strong>User:</strong> {todo.user.name}
+      </div>
+    )}
+  </div>
+);
